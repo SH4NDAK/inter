88ceab4e57a5892e0eea45ba8e2edd19303723df
Arthur Marena <112282554+SH4NDAK@users.noreply.github.com>
2024-05-17 16:20:29 -0300

    Rota de agenda

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, Row } from "react-bootstrap";
 
-function App() {
+
+export default function App() {
   return (
-    <div></div>
+    <Container fluid>
+      <Row>aaaa</Row>
+    </Container>
   );
 }
-
-export default App;
